5f55bf7738e7b0909c225b3ec1c4e5d77e1afcb7
khan <khan@khan.net>
2026-04-02 18:57:04 +0900

    patch 스크립트 수정

--- dev-root/bash.dev/aivax-install/patch.sh
+++ dev-root/bash.dev/aivax-install/patch.sh
@@ -3,25 +3,27 @@
 #rm -rf aivax-install.v1.0.1.$(date +%Y%m%d).tar.gz
 
 package_date=$(date +%Y%m%d%d%H%M)
-today=$(date +%Y%m%d)
+# today=$(date +%Y%m%d)
 
 aivax_ver=v1.0.1
 
-cp -rfv aivax-install aivax-install.${aivax_ver}.$package_date
-
-tar czvf aivax-install.${aivax_ver}.$today.tar.gz aivax-install.${aivax_ver}.$package_date
-
-#마지막. 날짜뒤에 hash 추가 => TODO: hash 전략 필요, hash로 git정보를 알수 있어야 한다.
-#version 파일을 생성 => aivax, pipeline, sslproxy 3개 조합의 파일
-file="aivax-install.${aivax_ver}.$today.tar.gz"
-
 #임시 방편, 우선 현재 파일의 hash를 구분할수 있도록 설정한다.
-hash=$(sha256sum "${file}" | awk '{print $1}' | cut -c1-6)
+#aivax의 패치 형상을 기준으로 hash를 만든다.
 
-new_file="aivax-install.${aivax_ver}.$today.${hash}.tar.gz"
+hash=$(tar -cf - aivax-install/aivax-patch/ | sha256sum | awk '{print $1}' | cut -c1-6)
 
-# 파일 이름 변경
-mv "$file" "$new_file"
+cp -rfv aivax-install aivax-install.${aivax_ver}.$package_date.${hash}
+
+tar czf aivax-install.${aivax_ver}.$package_date.${hash}.tar.gz aivax-install.${aivax_ver}.$package_date.${hash}
+
+# #마지막. 날짜뒤에 hash 추가 => TODO: hash 전략 필요, hash로 git정보를 알수 있어야 한다.
+# #version 파일을 생성 => aivax, pipeline, sslproxy 3개 조합의 파일
+# file="aivax-install.${aivax_ver}.$today.tar.gz"
+
+# new_file="aivax-install.${aivax_ver}.$today.${hash}.tar.gz"
+
+# # 파일 이름 변경
+# mv "$file" "$new_file"
 
 #마지막, 임시파일 삭제
-rm -rf aivax-install.${aivax_ver}.$package_date
+rm -rf aivax-install.${aivax_ver}.$package_date.${hash}
